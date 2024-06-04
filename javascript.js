d8e39f726523dfcaa6ff94b754d76633419819fc
CLBaxter-W <168162275+CLBaxter-W@users.noreply.github.com>
2024-06-03 20:15:02 -0600

    Added email contact form, email success page, auto return js

--- javascript.js
+++ javascript.js
@@ -1,10 +1,19 @@
 /* all the javascript you are ever going to want to see. */
 
-/* requirement for block09 1st draft personal website*/
-
+/* print the name of the page string passed into the function*/
 function iWantToPrintThePageName(pageNameSTR) {
   console.log("Hello World -" + " " + pageNameSTR);
   return;
 }
 
 console.log("Hello World - generic");
+
+/* Required javascript for contact form functinality */
+const form = document.querySelector("#form");
+const submitButton = document.querySelector("#submit");
+
+form.addEventListener("submit", (e) => {
+  submitButton.disabled = true;
+  e.preventDefault();
+  window.location.href = window.location.origin + "./emailSuccess.html";
+});
